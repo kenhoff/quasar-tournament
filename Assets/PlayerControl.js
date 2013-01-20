993b42f6d81aff4ab53cec3051ec2b91fc1e7ced
Ken Hoff <thekenhoff@gmail.com>
2013-01-19 17:06:10 -0700

    ship explosions

--- Assets/PlayerControl.js
+++ Assets/PlayerControl.js
@@ -4,6 +4,7 @@ public var ship_prefab : GameObject;
 public var camera_object : GameObject;
 public var camera_zoom : float;
 public var camera_zoom_rate : float;
+public var follow_ship : boolean;
 
 private var ship_control_script : ShipControl;
 
@@ -12,39 +13,58 @@ private var change_heading : float;
 
 
 function Start () {
+	CreateShip();
 
+	// bigger camera area for non-following ship
+
+	if (!follow_ship) camera_zoom = camera_zoom * 2;
+}
+
+function CreateShip () {
 	ship = Instantiate(ship_prefab, transform.position, transform.rotation);
 	ship.transform.parent = transform;
 	ship.name = ship_prefab.name;
 	ship_control_script = ship.GetComponent(ShipControl);
-
 }
 
 function Update () {
 
-	// control camera position
+	// respawn if ship is destroyed
 
-	camera_object.transform.position = ship.transform.position + Vector3(0, camera_zoom, 0);
+	if (Input.GetKeyDown(KeyCode.Backspace) && !ship) {
+		CreateShip();
+	}
+
+
+	// control camera position
+	if (follow_ship) {
+		camera_object.transform.position = ship.transform.position + Vector3(0, camera_zoom, 0);
+	}
+	else camera_object.transform.position = Vector3(0, camera_zoom, 0);
 
 	var change = -Input.GetAxis("Mouse ScrollWheel") * camera_zoom_rate;
 	camera_zoom += change;
 	if (camera_zoom < 1) camera_zoom = 1; 
 
 
+
 	// find angle between ship and mouse cursor
 
 	var cameraDif = camera_object.transform.position.y - transform.position.y;
 
-    var worldPos = camera_object.camera.ScreenToWorldPoint(Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraDif));
+	var worldPos = camera_object.camera.ScreenToWorldPoint(Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraDif));
 
-    // Debug.DrawLine(camera_object.transform.position, worldPos);
-    // Debug.DrawRay(ship.transform.position, ship.transform.forward * 5, Color.blue);
-    Debug.DrawRay(ship.transform.position, ship.rigidbody.velocity);
+	// Debug.DrawLine(camera_object.transform.position, worldPos);
+	// Debug.DrawRay(ship.transform.position, ship.transform.forward * 5, Color.blue);
+	Debug.DrawRay(ship.transform.position, ship.rigidbody.velocity);
 
-    var diff = worldPos - ship.transform.position;
-    var theta =  Mathf.Atan2(diff.x, diff.z) * Mathf.Rad2Deg;
+	var diff = worldPos - ship.transform.position;
+	var theta =  Mathf.Atan2(diff.x, diff.z) * Mathf.Rad2Deg;
 
-    change_heading = Mathf.DeltaAngle(ship.transform.eulerAngles.y, theta);
+	change_heading = Mathf.DeltaAngle(ship.transform.eulerAngles.y, theta);
+
+
+
 
 
 }
@@ -69,4 +89,10 @@ function FixedUpdate () {
 	var horizontal_input = Input.GetAxis("Horizontal");
 
 	ship_control_script.Maneuver(horizontal_input, vertical_input);
+}
+
+function OnGUI () {
+	GUI.Label (Rect (10, 10, 100, 20), "Shield: " + Mathf.Floor(ship_control_script.shield_health));
+	GUI.Label (Rect (10, 30, 100, 20), "Armor: " + Mathf.Floor(ship_control_script.armor_health));
+	GUI.Label (Rect (10, 50, 100, 20), "Hull: " + Mathf.Floor(ship_control_script.hull_health));
 }
--- Assets/ShipControl.js
+++ Assets/ShipControl.js
@@ -14,11 +14,16 @@ public var ship_projectile_prefab : GameObject;
 
 public var accuracy : float;
 
+public var max_shield : float;
+public var max_armor : float;
+public var max_hull : float;
+
 public var shield_health : float;
 public var armor_health : float;
 public var hull_health : float;
 
 public var shield_recharge_time : float;
+public var shield_recharge_rate : float;
 private var shield_downtime : float;
 
 private var shield_control_script : ShieldControl;
@@ -27,30 +32,59 @@ private var time_since_shot : float = 0;
 
 private var particles_count : int = 150;
 
+public var debris_object_prefab : GameObject;
+
 
 function Start () {
 	shield_control_script = ship_shield_object.GetComponent(ShieldControl);
 }
 
 function Update () {
-	Debug.Log(shield_health);
-	if (shield_health > 100) {
-		shield_health = 100;
+	// Debug.Log(shield_health);
+	if (shield_health > max_shield) {
+		shield_health = max_shield;
 	}
-	else shield_health += Time.deltaTime;
 
 	if (shield_health <= 0) {
-		ship_shield_object.SetActive(false);
+		shield_health = 0;
+		if (ship_shield_object.activeSelf) {
+			ship_shield_object.SetActive(false);
+		}
+	}
+
+	if ((shield_health > 0) && (!ship_shield_object.activeSelf)) {
+		ship_shield_object.SetActive(true);
+	}
+
+	if (shield_downtime >= shield_recharge_time) {
+		shield_health += Time.deltaTime * shield_recharge_rate;
+	}
+
+	shield_downtime += Time.deltaTime;
+	// Debug.Log(shield_downtime);
+
+	if (hull_health <= 0) {
+		Destroy();
 	}
 
 }
 
 function OnCollisionEnter (collisionInfo : Collision) {
-	// Debug.Log(collisionInfo.relativeVelocity.magnitude);
+
 	var damage = collisionInfo.relativeVelocity.magnitude;
-	if (shield_health < 0) {
-		shield_health -= damage;
+	if (damage >= shield_health) {
+		var armor_damage = damage - shield_health;
+		shield_health = 0;
+		shield_downtime = 0;
+		if (armor_damage > armor_health) {
+			var hull_damage = armor_damage - armor_health;
+			armor_health = 0;
+			hull_health -= hull_damage;
+		}
+		else armor_health -= armor_damage;
+
 	}
+	else shield_health -= damage;
 	shield_control_script.FlashOn();
 }
 
@@ -107,3 +141,14 @@ function Stabilize () {
 	rigidbody.AddForce(-rigidbody.velocity * ship_maneuver_force * .01);
 }
 
+function Destroy () {
+	var debris = Instantiate(debris_object_prefab, transform.position, transform.rotation);
+	for (var i = 0; i < debris.transform.GetChildCount(); i++) {
+		var debris_object = debris.transform.GetChild(i); 
+		debris_object.rigidbody.velocity = rigidbody.velocity;
+		debris_object.rigidbody.AddExplosionForce(300, transform.position, 300);
+		debris_object.rigidbody.AddTorque(Random.insideUnitSphere);
+	}
+	debris.particleSystem.Emit(500);
+	Destroy(gameObject);
+}
